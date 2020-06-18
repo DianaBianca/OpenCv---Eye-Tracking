550645a5ab433c59714e85780a45d0d16247b5ed
diana <diana.iruna@gmail.com>
2020-06-18 11:02:16 -0300

    organizando o material

--- animação/animacao.py
+++ animação/animacao.py
@@ -1,35 +1,28 @@
 import pygame
 import ctypes
- 
-# Define some colors
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-GREEN = (0, 255, 0)
-RED = (255, 0, 0)
- 
+import time
+import os
+
+# tela cheia 
+os.environ['SDL_VIDEO_CENTERED'] = '1'
 pygame.init()
  
-# Set the width and height of the screen [width, height]
+# Define algumas cores colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+ 
+#Seta a largura e a altura da tela utilizada [width, height](só funciona para windows)
 user32 = ctypes.windll.user32
 
 #1366 768- tamanho da minha tela
 sizeX = user32.GetSystemMetrics(0)
-sizeY = user32.GetSystemMetrics(1) # tirando um pedaço que ultrapassa a tela
+sizeY = user32.GetSystemMetrics(1) 
 
 size = sizeX,sizeY
 screen = pygame.display.set_mode(size)
-#print(sizeY)
-#print(sizeX)
 
-x = int(sizeX/2)
-y = int(sizeY/2)
-
-inicio = 25
-
-nextX = x - inicio 
-nextY = y - inicio 
-print(nextX)
-print(nextY)
+nextx = int(sizeX/2) - 20
+nexty = int(sizeY/2)
 
 #nome da janela
 pygame.display.set_caption("Calibração")
@@ -40,10 +33,20 @@ done = False
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()
 
-i = 1
-py = 25
-px = 25
-speed = 3
+i = 2
+py = 0
+px = 0
+direita = True
+voltar = False
+# como o relógio do pygame trabalha
+# em milissegundos, dividimos por 1000
+# para manter os 100 pixels por segundo
+velocity = 0.05
+
+
+# criamos uma instância do relógio
+clock = pygame.time.Clock()
+
 # -------- Main Program Loop -----------
 
 while done == False:
@@ -52,39 +55,44 @@ while done == False:
             done = True  # Flag that we are done so we exit this loop
 
     # Set the screen background
-    screen.fill(BLACK)
-    
-    pygame.draw.circle(screen,GREEN, [px,py], 25)
-    pygame.display.flip()
-    
-    if(i <= 3):    
-        px += nextX
-        #pygame.display.flip()
-        clock.tick(0.5)
-        print(px, py)
-        
-    if((i < 7) and (i > 3)):
-        py = y
-        px -= nextX
-        #pygame.display.flip()
-        clock.tick(0.5)
-        print(px, py)
+    #screen.fill(BLACK)
+    # chamamos o tick do relógio para 30 fps
+    # e armazenamos o delta de tempo
+    dt = clock.tick(20)
 
-    if(i == 7):
-        py = x
-        px += 25
-        print(px, py)
+    while i != 11 :
+
+        event = pygame.event.poll()
+
+        if event.type == pygame.QUIT:
+            break
+        screen.fill(BLACK)
         
-        clock.tick(0.5)
+        pygame.draw.rect(screen,(255,255,0), [px, py, 40, 40])#
         
-    if(i > 7):
-        py = x
-        px += nextX
-        print(px, py)
-        #pygame.display.flip()
-        clock.tick(0.5)
-    
-    i = i+1
-   
+        pygame.display.flip()
+        
+        time.sleep(5)#tempo para a calibração de cada ponto
+        
+        if( i < 4 ):        
+            px += nextx            
+            
+        elif(i < 7):
+            if(i == 4):
+                print('i == 4')
+                py += nexty - 20
+               
+            else:
+                px -= nextx 
+                         
+        elif(i > 6) :            
+            if(i == 7):
+                py += nexty - 30
+                print('i == 7')
+                
+            else:
+                px += nextx 
+        i += 1           
+
 # Close the window and quit.
 pygame.quit()
