a98f26089322dee7775e5b3de19d4ed3e645c922
diana <diana.iruna@gmail.com>
2020-06-18 12:35:32 -0300

    coordenadas dos points ok

--- calibração/calibracao.py
+++ calibração/calibracao.py
@@ -2,36 +2,35 @@ import pygame
 import ctypes
 import time
 import os
-import sys
 import numpy as np
 
-# tela cheia 
+# tela cheia
 os.environ['SDL_VIDEO_CENTERED'] = '1'
 pygame.init()
- 
+
 # Define algumas cores colors
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
- 
-#Seta a largura e a altura da tela utilizada [width, height](só funciona para windows)
+
+# Seta a largura e a altura da tela utilizada [width, height](só funciona para windows)
 user32 = ctypes.windll.user32
 
-#1366 768- tamanho da minha tela
+# 1366 768- tamanho da minha tela
 sizeX = user32.GetSystemMetrics(0)
-sizeY = user32.GetSystemMetrics(1) 
+sizeY = user32.GetSystemMetrics(1)
 
-size = sizeX,sizeY
+size = sizeX, sizeY
 screen = pygame.display.set_mode(size)
 
-nextx = int(sizeX/2) - 20
-nexty = int(sizeY/2)
+nextx = int(sizeX / 2) - 20
+nexty = int(sizeY / 2)
 
-#nome da janela
+# nome da janela
 pygame.display.set_caption("Calibração")
- 
+
 # Loop until the user clicks the close button.
 done = False
- 
+
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()
 
@@ -47,7 +46,9 @@ velocity = 0.05
 # criamos uma instância do relógio
 clock = pygame.time.Clock()
 
+#armazena as coordenadas da animação
 points = []
+
 # -------- Main Program Loop -----------
 
 while done == False:
@@ -56,43 +57,45 @@ while done == False:
             done = True  # Flag that we are done so we exit this loop
 
     # Set the screen background
-    #screen.fill(BLACK)
+    # screen.fill(BLACK)
     # chamamos o tick do relógio para 30 fps
     # e armazenamos o delta de tempo
     dt = clock.tick(20)
 
-    while i != 11 :
+    while i != 11:
 
         event = pygame.event.poll()
 
         if event.type == pygame.QUIT:
             break
         screen.fill(BLACK)
-        
-        pygame.draw.rect(screen,(255,255,0), [px, py, 40, 40])
+
+        pygame.draw.rect(screen, (255, 255, 0), [px, py, 40, 40])
 
         pygame.display.flip()
 
-        points.append([px,py])
+        points.append([px, py])
 
         time.sleep(5) #tempo para a calibração de cada ponto
-        
-        if( i < 4 ):        
-            px += nextx            
-            
-        elif(i < 7):
-            if(i == 4):
+
+        if (i < 4):
+            px += nextx
+
+        elif (i < 7):
+            if (i == 4):
+                print('i == 4')
                 py += nexty - 20
-               
+
             else:
-                px -= nextx 
-                         
-        elif(i > 6) :            
-            if(i == 7):
+                px -= nextx
+
+        elif (i > 6):
+            if (i == 7):
                 py += nexty - 30
-                
+                print('i == 7')
+
             else:
-                px += nextx 
+                px += nextx
         i += 1
     done = True
     # Close the window and quit.
