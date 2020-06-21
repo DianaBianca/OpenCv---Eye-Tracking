a5cb6326339ee0c4bac73505674fcd8f1f80594f
diana <diana.iruna@gmail.com>
2020-06-21 15:56:52 -0300

    coordenadas dos targets ok

--- threads.py
+++ threads.py
@@ -83,15 +83,10 @@ class myThread (threading.Thread):
                   else:
                       px += nextx
               i += 1
-             
-          
          
-          # Close the window and quit.
-          
-          
+          # Close the window and quit
           done = True
-          pygame.quit()
-          print("cabou")
+          
       global vetTarget
       vetTarget = vet.getVet()
 
@@ -150,8 +145,52 @@ threads.append(thread1)
 # Wait for all threads to complete
 for t in threads:
     t.join()
-print ("Exiting Main Thread")
+
+pygame.quit()
 
 targets = np.array(np.asarray(vetTarget))
-print("Array de targets funcionando aaaaa CARALHOOO ----->  \n ",targets)
+
+#coordenadas dos olhos
+eyes = np.array([[0, 0, 1],
+                 [0.25, 0, 1],
+                 [0.5, 0, 1],
+                 [0, 0.25, 1],
+                 [0.25, 0.25, 1],
+                 [0.5, 0.25, 1],
+                 [0, 0.50, 1],
+                 [0.25, 0.50, 1],
+                 [0.5, 0.50, 1]])
+
+tamanhofixo = 60  # tamanho dos targets
+
+# plt.scatter(eyes[:,0],eyes[:,1], color='blue', s=tamanhofixo)
+plt.scatter(targets[:, 0], targets[:, 1], color='red', s=tamanhofixo)
+
+equation = np.ones((9, 6))  # 6 equações e 9 alvos
+
+# pegar cada coordenada do olho e realizar a equação
+for i, eye in enumerate(eyes):
+    equation[i, :-1] = [eye[0] ** 2, eye[1] ** 2, eye[0] * eye[1], eye[0], eye[1]]
+
+coeffsX = np.linalg.pinv(equation).dot(targets[:, 0])
+coeffsY = np.linalg.pinv(equation).dot(targets[:, 1])
+
+M = np.vstack((coeffsX, coeffsY))
+
+eye = np.array([0.25, 0.15])
+
+# Gaze estimation method
+gaze = M.dot([eye[0] ** 2, eye[1] ** 2, eye[0] * eye[1], eye[0], eye[1], 1])
+
+print("gaze --> ",gaze)
+
+a = gaze[0]
+b = gaze[1]
+
+plt.scatter(a, b, color='black', s=200)
+
+plt.xlabel("x")
+plt.ylabel("y")
+plt.show()
+
 
